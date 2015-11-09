aaa0461485b0b67db5882b6521cea5e5d7c06429
Anthony Romaniello <aromaniello70@gmail.com>
2015-11-08 22:24:07 -0500

    Fixed cname error

--- index.php
+++ index.php
@@ -190,7 +190,7 @@
             }
         }
 
-        function getCookie() {
+        function getCookie(cname) {
             var name = cname + "=";
             var ca = document.cookie.split(';');
             for (var i = 0; i < ca.length; i++) {
